e6c084ad27fbfaa6e5c62751d2e676343d1e92a7
pmiranda27 <pmiranda.senai29@gmail.com>
2025-06-01 16:34:22 -0300

    Feat: Página inicial com JS da navbar e do header no geral.

---
+++ JS/estilos.js
@@ -0,0 +1,27 @@
+const header_element = document.getElementById('header-global');
+const navbar_element = document.getElementById('navbar-selector');
+const menu_hamburguer_selector = document.getElementById('menu-hamburguer-selector');
+
+let navbarAberta = 0;
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY == 0){
+        header_element.className = ""
+    }
+    else if (window.scrollY > 0){
+        header_element.className = "header-transparente-baixo"
+    }
+});
+
+menu_hamburguer_selector.addEventListener('click', () => {
+    if(navbarAberta == 0) {
+        navbarAberta = 1;
+        navbar_element.className = "navbar-links navbar-links-open"
+        menu_hamburguer_selector.src = "Assets/Icons/menu_fechar_branco.png"
+    }
+    else if(navbarAberta == 1) {
+        navbarAberta = 0;
+        navbar_element.className = "navbar-links"
+        menu_hamburguer_selector.src = "Assets/Icons/menu_hamburguer_branco.png"
+    }
+});
