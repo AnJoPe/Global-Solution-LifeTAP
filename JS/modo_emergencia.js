ee77338417d2c1e4ef9dce2c88dbe7a8db6d7a18
pmiranda27 <pmiranda.senai29@gmail.com>
2025-06-02 22:59:31 -0300

    Feat: Página de Modo Emergência.

---
+++ JS/modo_emergencia.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const opcoes = document.querySelectorAll(".escolha-emergencia");
+    const mensagemDiv = document.getElementById("mensagem-simulacao");
+
+    opcoes.forEach((opcao, index) => {
+        opcao.addEventListener("click", () => {
+            let servico = index === 0 ? "Polícia" : "Bombeiros";
+
+            mensagemDiv.textContent = `Simulação de chamada para ${servico} realizada com sucesso.`;
+            mensagemDiv.classList.remove("escondido");
+
+            setTimeout(() => {
+                mensagemDiv.classList.add("escondido");
+            }, 5000);
+        });
+    });
+});
