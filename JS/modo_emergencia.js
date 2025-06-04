b60b7940b3e1626c6ac8512ad347a61faee0c7e3
AndreColombo <andrerosacolombo18+github@gmail.com>
2025-06-04 00:40:37 -0300

    Fix: Responsividade TOTAL Small

--- JS/modo_emergencia.js
+++ JS/modo_emergencia.js
@@ -2,16 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
     const opcoes = document.querySelectorAll(".escolha-emergencia");
     const mensagemDiv = document.getElementById("mensagem-simulacao");
 
+    var timeout_sumir
+
     opcoes.forEach((opcao, index) => {
         opcao.addEventListener("click", () => {
+            clearTimeout(timeout_sumir);
+
             let servico = index === 0 ? "Polícia" : "Bombeiros";
 
             mensagemDiv.textContent = `Simulação de chamada para ${servico} realizada com sucesso.`;
             mensagemDiv.classList.remove("escondido");
 
-            setTimeout(() => {
+            
+            timeout_sumir = setTimeout(() => {
                 mensagemDiv.classList.add("escondido");
-            }, 7000);
+            }, 5000);
         });
     });
 });
